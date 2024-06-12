5a96e0357b0a3805dd0c8b73940fba8115cd0710
Cyprian Munene <munenecyp@gmail.com>
2024-06-12 16:43:40 -0700

    allow all

--- titan/settings.py
+++ titan/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ji%4b(7!00-=&i-xy8e(ls7y7d5)*e2u^j)2sg)np5*m$e-!uq'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
